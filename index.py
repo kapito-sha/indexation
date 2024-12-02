7281d370cf1e40badbcbc4f0b5d6b9531376dcc0
kapitosha <lunevalubasa@gmail.com>
2024-12-03 01:14:46 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,14 @@
+example = 'family'
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1::2])
+# я не поняла, приведен ли в задании "топинамбур" как пример того, что должно получиться,
+# или это тоже нужно повторить. на всякий случай сделала
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
